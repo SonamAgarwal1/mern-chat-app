a67c7bfff24e1f0dccb5f8f5d9db98517b524f37
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-29 00:29:02 +0530

    Sign Up Endpoint-Controller and Model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,44 @@
-export const signup = (req, res) => {
-  console.log("signup");
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password dont match" });
+    }
+    const user = await User.findOne({ username }); //check in DB if username exists
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    //HASH Password here
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save(); //save it to the DB
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
+  res.send("Loggedin");
   console.log("login");
 };
 
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body in auth.controller)
+
+// app.get("/", (req, res) => {
+//   res.json({ msg: "Hello world!!!" });
+// });
+
 app.get("/", (req, res) =>
   //root route http://localhost:5000
   res.send("Hello world")
