2c381404759d90bf997821d273d5143849f8bc31
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-28 22:40:27 +0530

    Connecting to MongoDB

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-  res.send("Login Route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -9,4 +10,7 @@ app.get("/", (req, res) =>
   res.send("Hello world")
 );
 app.use("/api/auth", authRoutes);
-app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on Port ${PORT}`);
+});
