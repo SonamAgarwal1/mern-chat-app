e60ad7b868d5047be8b55c9405c949387d48d86a
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-29 00:49:41 +0530

    hashpassword with help of bcryptjs library

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
@@ -12,6 +13,8 @@ export const signup = async (req, res) => {
     }
 
     //HASH Password here
+    const salt = await bcryptjs.genSalt(10); //The higher it is it will be more secure and at the same time will be slower
+    const hashpassword = await bcryptjs.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -19,7 +22,7 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashpassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
