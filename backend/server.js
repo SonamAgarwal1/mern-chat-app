cce091c5b1868bfe672f7e49697a0cea416b32a0
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-28 19:33:22 +0530

    Inital setup with routes in backedn

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) =>
+  //root route http://localhost:5000
+  res.send("Hello world")
+);
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
