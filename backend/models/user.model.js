c3c9dccee5873b1a03b5505bdfaaa032ad3b111e
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-03-02 01:31:22 +0530

    Conversation model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,30 +1,34 @@
 import mongoose from "mongoose";
 
-const userScheme = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userScheme = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female"],
-  },
-  profilePic: {
-    type: String,
-    default: "",
-  },
-});
+  //createdAt,updatedAt=>Member since <updatedAt>
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userScheme); //User has to be capital
 
