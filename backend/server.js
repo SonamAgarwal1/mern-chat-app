e6590c48b948401c869a0a767db6929a9473bea1
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-03-02 21:08:23 +0530

    Message Controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body in auth.controller)
+app.use(cookieParser()); // To be able to read cookies
 
 // app.get("/", (req, res) => {
 //   res.json({ msg: "Hello world!!!" });
@@ -19,6 +24,8 @@ app.get("/", (req, res) =>
   res.send("Hello world")
 );
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on Port ${PORT}`);
