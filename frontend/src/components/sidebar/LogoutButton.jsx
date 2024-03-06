aaecb19541c5f81eb4c08c50a6c1271cb07a3c74
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-03-06 21:00:44 +0530

    Loader

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -2,13 +2,17 @@ import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-  const { logout } = useLogout();
+  const { loading, logout } = useLogout();
   return (
     <div className="mt-auto">
-      <BiLogOut
-        className="w-6 h-6 text-white cursor-pointer"
-        onClick={logout}
-      />
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
   );
 };
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import useLogin from "../../hooks/useLogin";
+import { useAuthContext } from "../../context/AuthContext";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useLogin();
-
+  const { loading } = useAuthContext();
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(password, username);
@@ -54,7 +55,16 @@ const Login = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2">Login</button>
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
+            </button>
           </div>
         </form>
       </div>
