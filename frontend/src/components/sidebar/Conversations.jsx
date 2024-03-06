817bf6fcd1c4d247539b8cdd6dd0be5e340e1d44
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-03-06 22:05:04 +0530

    Retrieving username and avatar from api

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,22 @@
+import useGetConversations from "../../hooks/useGetConversations";
 import Conversation from "./Conversation";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, index) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={index === conversations.length - 1} // For avoiding divider on last conversation
+        />
+      ))}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 };
