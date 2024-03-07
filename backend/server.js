0e67bce6aeb09bb966a3fb13e1ca853fa648748a
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-03-08 01:10:38 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,24 +12,24 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
-app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body in auth.controller)
-app.use(cookieParser()); // To be able to read cookies
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-// app.get("/", (req, res) => {
-//   res.json({ msg: "Hello world!!!" });
-// });
-
-app.get("/", (req, res) =>
-  //root route http://localhost:5000
-  res.send("Hello world")
-);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server running on Port ${PORT}`);
+  console.log(`Server Running on port ${PORT}`);
 });
